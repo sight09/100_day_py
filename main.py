ccde83fca2a7706c8ab533608bfd4bccafb24e07
Amanuel <zeamanuel09@gmail.com>
2026-03-16 23:58:29 +0300

    some code is added

--- main.py
+++ main.py
@@ -1064,61 +1064,72 @@
 
 
 
-#-------gusse the number game ------------
+# #-------gusse the number game ------------
 
-from random import randint
+# from random import randint
 
-EASY_LEVEL_TURNS = 10
-HARD_LEVEL_TURNS = 5
-
-
-
-def check_answer(gusse, num, turns):
-    if num < gusse:
-        print("it is too high")
-        return turns -1
-    elif num > gusse:
-        print("it's to low")
-        return turns-1    
-    elif num == gusse:
-        print("you got it")
+# EASY_LEVEL_TURNS = 10
+# HARD_LEVEL_TURNS = 5
 
 
-def set_difficulty():
-    level = input("Choose a difficulty. Type 'essy' or 'hard' : ")
-    if level == "essy":
-        return  EASY_LEVEL_TURNS
-    else:
-        return HARD_LEVEL_TURNS
+
+# def check_answer(gusse, num, turns):
+#     if num < gusse:
+#         print("it is too high")
+#         return turns -1
+#     elif num > gusse:
+#         print("it's to low")
+#         return turns-1    
+#     elif num == gusse:
+#         print("you got it")
 
 
-def game():
+# def set_difficulty():
+#     level = input("Choose a difficulty. Type 'essy' or 'hard' : ")
+#     if level == "essy":
+#         return  EASY_LEVEL_TURNS
+#     else:
+#         return HARD_LEVEL_TURNS
 
-    print("Welcome to the Number Guessing Game ! ")
-    print("I'm thinking of a number between 1 and 100.")
-    num = randint(1, 100)
-    print(num)
+
+# def game():
+
+#     print("Welcome to the Number Guessing Game ! ")
+#     print("I'm thinking of a number between 1 and 100.")
+#     num = randint(1, 100)
+#     print(num)
 
 
-    turns = set_difficulty()
+#     turns = set_difficulty()
     
 
-    gusse = 0
-    while gusse != num:
-        print(f"you have {turns} attempts remaning to guess to the number ")
-        gusse = int(input("gusse a  : "))
+#     gusse = 0
+#     while gusse != num:
+#         print(f"you have {turns} attempts remaning to guess to the number ")
+#         gusse = int(input("gusse a  : "))
 
 
-        turns = check_answer(gusse, num, turns)
-        if turns == 0:
-            print("you've run out of gusses, you lose")
-            return
-        elif turns != num:
-            print("GUSSE AGAIN")
+#         turns = check_answer(gusse, num, turns)
+#         if turns == 0:
+#             print("you've run out of gusses, you lose")
+#             return
+#         elif turns != num:
+#             print("GUSSE AGAIN")
             
             
             
-game()
+# game()
 
 
 
+
+
+for num in range(1,100):
+    if num % 3 == 0 and num % 5 ==0 :
+        print("fizzbuzz")  
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
