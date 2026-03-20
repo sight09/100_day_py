ffdc7b2077d3fb6b3ed6dea4b3e910191d2be02d
Amanuel <zeamanuel09@gmail.com>
2026-03-20 21:45:38 +0300

    shape drawer is added

--- shape_draw/main.py
+++ shape_draw/main.py
@@ -1,3 +1,22 @@
+from turtle import Screen
+import turtle as t
+import random
+
+
+tin = t.Turtle()
+color = ["red", "green", "blue", "black", "yellow", "purpule"]
+
+def draw_shape(num_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        tin.forward(100)
+        tin.right(angle)
+
+for shape_side_n in range(3, 11):
+    tin.color(random.choice(color))
+    draw_shape(shape_side_n)
 
 
 
+my_screen = Screen()
+my_screen.exitonclick()
